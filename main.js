c4a92d1f2e730c7f0a5ea54121e481609a547802
Bodi_Ali <bodiali59@gmail.com>
2023-12-18 13:50:24 +0200

    debug loading screen

--- main.js
+++ main.js
@@ -20,16 +20,24 @@ pass.addEventListener("input", validate);
 confirmPass.addEventListener("input", validate);
 
 document.addEventListener("DOMContentLoaded", function () {
-  var loaderOverlay = document.querySelector(".loader-overlay");
-  loaderOverlay.style.opacity = 0; // Start with full opacity
+  let loaderOverlay = document.querySelector(".loader-overlay");
+  let video = document.querySelector("#video");
+  function fadeOutLoader() {
+    let fadeOutInterval = setInterval(function () {
+      if (loaderOverlay.style.opacity > 0) {
+        loaderOverlay.style.opacity -= 0.01;
+      } else {
+        clearInterval(fadeOutInterval);
+        loaderOverlay.style.display = "none";
+      }
+    }, 10);
+  }
 
-  // Fading out the loader overlay
-  var fadeOutInterval = setInterval(function () {
-    if (loaderOverlay.style.opacity > 0) {
-      loaderOverlay.style.opacity -= 0.01; // Adjust the fade speed if needed
-    } else {
-      clearInterval(fadeOutInterval);
-      loaderOverlay.style.display = "none";
-    }
-  }, 10); // Adjust the interval for smoother fading
+  video.addEventListener("loadeddata", function () {
+    fadeOutLoader();
+  });
+
+  setTimeout(function () {
+    fadeOutLoader();
+  }, 10000);
 });
